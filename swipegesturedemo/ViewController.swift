7a4f9294b05f61429e04c9a729ddfa858366978f
simranjeet kaur <chakkalsimmi@gmail.com>
2020-01-07 20:38:28 -0500

    finalcommit

--- swipegesturedemo/ViewController.swift
+++ swipegesturedemo/ViewController.swift
@@ -12,9 +12,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let swiperight = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        
+        swiperight.direction = UISwipeGestureRecognizer.Direction.right
+        view.addGestureRecognizer(swiperight)
+        
+        
+        let swipeleft = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+               
+               swipeleft.direction = UISwipeGestureRecognizer.Direction.left
+               view.addGestureRecognizer(swipeleft)
         // Do any additional setup after loading the view.
     }
 
+    @objc func swiped(gesture: UISwipeGestureRecognizer){
+        
+      let swipegesture = gesture as UISwipeGestureRecognizer
+            switch swipegesture.direction {
+            case UISwipeGestureRecognizer.Direction.right:
+                print("right swipe")
+                case UISwipeGestureRecognizer.Direction.left:
+                print("left swipe")
+            default:
+                break
+            }
+        }
+        
+    }
 
-}
 
